45a1757de2be926b3936b972546e334bb448fe52
Maromug <marlon08.04.2007@gmIL.com>
2024-11-06 19:37:13 -0500

    Segundo commit v2

--- main.js
+++ main.js
@@ -2,17 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3203;
 const path = require('path');
+const colors = require('colors')
+ 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'))
+    res.render('Login')
+    console.log("Yendo al login".green.bgBlue)
 })
 
 app.get('/welcom', (req,res)=>{
-    res.sendFile(path.join(__dirname,'welcom.html'))
+    res.render('welcom')
+    console.log('Llendo al menu de bienvenida'.cyan.bgWhite)
 })
 
 app.get('/datos', (req,res)=>{
-    res.sendFile(path.join(__dirname,'datos.html'))
+    res.render('datos')
+    console.log('Llendo al menu de bienvenida'.magenta.bgYellow)
 })
 
 app.listen(port)
